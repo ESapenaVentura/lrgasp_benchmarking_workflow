4f893d91e3db8b4d318a03aad492eb807723bf4f
ESapenaVentura <sapenaventura@gmail.com>
2023-07-21 08:53:21 +0100

    Added oeb vre run files for v2.1.6

--- oeb_vre_run/materialize-data.sh
+++ oeb_vre_run/materialize-data.sh
@@ -22,7 +22,8 @@ case "$scriptdir" in
 esac
 
 datasetsdir="${scriptdir}/reference_datasets"
-inputsdir="${scriptdir}/input_dataset"
+inputsdir="${scriptdir}/lrgasp_data"
+mkdir $inputsdir
 
 repodir="${scriptdir}/the_repo"
 cleanup() {
@@ -40,23 +41,23 @@ cleanup
 set -e
 #rm -rf "${datasetsdir}" "${inputsdir}"
 
-git_repo="$(jq -r '.arguments[] | select(.name=="nextflow_repo_uri") | .value' "${scriptdir}"/config.json)"
-git_tag="$(jq -r '.arguments[] | select(.name=="nextflow_repo_tag") | .value' "${scriptdir}"/config.json)"
-git clone -n "${git_repo}" "${repodir}"
-cd "${repodir}" && git checkout "${git_tag}"
-
-echo
 echo "Fetching reference dataset for ${DATA_RELEASE}"
 
-# Clone docker repository - Example data was deposited there
-wget https://github.com/ESapenaVentura/lrgasp_docker/raw/main/example_data/iso_detect_ref_input_example/input_user_files.tar.gz && mv input_user_files.tar.gz "$inputsdir"/input_user_files.tar.gz
+mkdir "$inputsdir"/input_data
+# Clone the already existing data from the lrgasp docker
+git clone https://github.com/ESapenaVentura/lrgasp_docker.git
+mv lrgasp_docker/data/ "$inputsdir"/aggregation_dir/
+mv lrgasp_docker/example_data/iso_detect_ref_input_example/input_user_files.tar.gz "$inputsdir"/input_data/input_user_files.tar.gz
+# Delete repository
+rm -r lrgasp_docker
 
 
-# Download reference genome and transcriptome
-wget https://lrgasp.s3.amazonaws.com/lrgasp_grcm39_sirvs.fasta && mv lrgasp_grcm39_sirvs.fasta "$inputsdir"/lrgasp_grcm39_sirvs.fasta
-synapse get syn26986100 && mv lrgasp_gencode_vM28_sirvs.mouse.gtf "$inputsdir"/lrgasp_gencode_vM28_sirvs.mouse.gtf
+# Download reference files
+mkdir "$inputsdir"/input_data/public_ref
+wget https://lrgasp.s3.amazonaws.com/lrgasp_grcm39_sirvs.fasta && mv lrgasp_grcm39_sirvs.fasta "$inputsdir"/input_data/public_ref/lrgasp_grcm39_sirvs.fasta
+synapse get syn26986100 && mv lrgasp_gencode_vM28_sirvs.mouse.gtf "$inputsdir"/input_data/public_ref/lrgasp_gencode_vM28_sirvs.mouse.gtf
+synapse get syn26986190 && mv es_rep1SJ.out.tab "$inputsdir"/input_data/public_ref/es_rep1SJ.out.tab
+synapse get syn27249518 && mv mouse.refTSS_v3.1.mm39.bed "$inputsdir"/input_data/public_ref/mouse.refTSS_v3.1.mm39.bed
 
-# Move the rest of the data
-cp -r lrgasp_data/ "${datasetsdir}"
 
 
